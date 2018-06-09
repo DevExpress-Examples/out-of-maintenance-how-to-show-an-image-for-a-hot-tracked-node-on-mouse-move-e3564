d0dc1d387ca878ea902cdc984f8f8728b86ea024
DevExpressExampleBot <fakeEmail>
2018-06-09 18:50:13 +0300

    Source auto update [skip ci]

--- VB/TreeListDrawImage/DrawHelper.vb
+++ VB/TreeListDrawImage/DrawHelper.vb
@@ -10,10 +10,10 @@ Imports System.Drawing
 
 Namespace TreeListDrawImage
     Public Class DrawHelper
-        Private TreeList As TreeList
+        Private fTreeList As TreeList
 
         Public Sub RegisterTreeList(ByVal treeList As TreeList)
-            Me.TreeList = treeList
+            Me.fTreeList = treeList
             SubscribeEvents()
         End Sub
 
@@ -21,37 +21,54 @@ Namespace TreeListDrawImage
             RegisterTreeList(treeList)
             Me.Image = image
         End Sub
+        Public Sub Unregister()
+            UnsubscribeEvents()
+            fTreeList = Nothing
+        End Sub
 
         Public Property Image() As Image
 
         Public Event ImageClick As EventHandler
 
         Private Sub SubscribeEvents()
-            AddHandler TreeList.CustomDrawNodeIndicator, AddressOf treeList1_CustomDrawNodeIndicator
-            AddHandler TreeList.Click, AddressOf treeList1_Click
-            AddHandler TreeList.MouseMove, AddressOf treeList1_MouseMove
+            AddHandler fTreeList.CustomDrawNodeIndicator, AddressOf treeList1_CustomDrawNodeIndicator
+            AddHandler fTreeList.Click, AddressOf treeList1_Click
+            AddHandler fTreeList.MouseMove, AddressOf treeList1_MouseMove
+        End Sub
+        Private Sub UnsubscribeEvents()
+            RemoveHandler fTreeList.CustomDrawNodeIndicator, AddressOf treeList1_CustomDrawNodeIndicator
+            RemoveHandler fTreeList.Click, AddressOf treeList1_Click
+            RemoveHandler fTreeList.MouseMove, AddressOf treeList1_MouseMove
         End Sub
 
         Private Sub treeList1_CustomDrawNodeIndicator(ByVal sender As Object, ByVal e As CustomDrawNodeIndicatorEventArgs)
-            TreeList.ViewInfo.PaintAppearance.Row.FillRectangle(e.Cache, e.Bounds)
-            If (TryCast(TreeList.ViewInfo.RowsInfo(e.Node), RowInfo)).Bounds.Contains(TreeList.PointToClient(Control.MousePosition)) Then
-                e.Graphics.DrawImage(Image, e.Bounds.Location)
+            Dim tree As TreeList = TryCast(sender, TreeList)
+            Dim backBrush As Brush = tree.ViewInfo.PaintAppearance.Row.GetBackBrush(e.Cache)
+            e.Cache.FillRectangle(backBrush, e.Bounds)
+            If node Is e.Node Then
+                e.Cache.DrawImage(Image, e.Bounds.Location)
             End If
             e.Handled = True
         End Sub
 
         Private Sub treeList1_Click(ByVal sender As Object, ByVal e As EventArgs)
-            If TreeList.CalcHitInfo(TreeList.PointToClient(Control.MousePosition)).HitInfoType = HitInfoType.RowIndicator Then
+            Dim tree As TreeList = TryCast(sender, TreeList)
+            Dim pt As Point = tree.PointToClient(Control.MousePosition)
+            Dim hitInfo = tree.CalcHitInfo(pt)
+            If hitInfo.HitInfoType = HitInfoType.RowIndicator Then
                 RaiseEvent ImageClick(Me, EventArgs.Empty)
             End If
         End Sub
 
         Private node As TreeListNode
         Private Sub treeList1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
-            Dim currentNode As TreeListNode = TreeList.CalcHitInfo(TreeList.PointToClient(Control.MousePosition)).Node
+            Dim tree As TreeList = TryCast(sender, TreeList)
+            Dim hitInfo = tree.CalcHitInfo(e.Location)
+            Dim currentNode As TreeListNode = hitInfo.Node
             If currentNode IsNot node Then
+                tree.InvalidateNode(node)
                 node = currentNode
-                TreeList.InvalidateNodes()
+                tree.InvalidateNode(node)
             End If
         End Sub
     End Class
--- VB/TreeListDrawImage/Form1.vb
+++ VB/TreeListDrawImage/Form1.vb
@@ -9,10 +9,11 @@ Namespace TreeListDrawImage
     Partial Public Class Form1
         Inherits Form
 
+        Private helper As DrawHelper
         Public Sub New()
             InitializeComponent()
             AddNodes()
-            Dim helper As New DrawHelper()
+            helper = New DrawHelper()
             helper.RegisterTreeList(treeList1, My.Resources.Asphalt_World___16x16)
             AddHandler helper.ImageClick, AddressOf helper_ImageClick
         End Sub
@@ -25,6 +26,11 @@ Namespace TreeListDrawImage
             For i As Integer = 0 To 9
                 treeList1.AppendNode(New Object() { String.Format("Node {0}", i) }, -1)
             Next i
+        End Sub
+        Protected Overrides Sub OnFormClosing(ByVal e As FormClosingEventArgs)
+            RemoveHandler helper.ImageClick, AddressOf helper_ImageClick
+            helper.Unregister()
+            MyBase.OnFormClosing(e)
         End Sub
     End Class
 End Namespace
